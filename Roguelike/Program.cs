739a686f05a7261c3f831da07ab4f9b25d5c2e3d
trollingchar <rodind-1@mail.ru>
2018-12-26 17:15:37 +0300

    initial

--- Roguelike/Program.cs
+++ Roguelike/Program.cs
@@ -1,8 +1,27 @@
-﻿namespace Roguelike {
+﻿using Microsoft.Xna.Framework;
+
+
+namespace Roguelike {
 
     public class Program {
 
-        public static void Main (string [] args) {}
+        public static void Main (string [] args) {
+            using (var game = new Game ()) {
+                game.Run ();
+            }
+        }
+
+    }
+
+
+    public class Game : Microsoft.Xna.Framework.Game {
+
+        private GraphicsDeviceManager _graphicsDevice;
+
+
+        public Game () {
+            _graphicsDevice = new GraphicsDeviceManager(this);
+        }
 
     }
 
